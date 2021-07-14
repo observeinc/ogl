5377c0e0706ae8580e00b9acdb29541e77c4efe6
Deepak Chawla <deepak@observeinc.com>
2021-07-14 07:50:06 +0000

    Ignore references of pointer dereferenced fields
    A pointer deferenced nested field of a range variable is the same as the
    nested field in the container element, so its ok to take its reference.
    Modifications to such field are not modifying the copy but the actual
    data in the container element.

--- pkg/analyzer/range_var_ref.go
+++ pkg/analyzer/range_var_ref.go
@@ -4,21 +4,23 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
+	"go/types"
 
+	"github.com/davecgh/go-spew/spew"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
 )
 
 const (
-	expectedMaxNesting = 5
+	expectedMaxLoopNesting = 5
 )
 
 func init() {
 	registerAnalyzer(getRangeVarRefAnalyzer())
 }
 
-// getRangeVarRefAnalyzer builds and returns an Analyzer for the defer-in-loop check.
+// getRangeVarRefAnalyzer builds and returns an Analyzer for the range-variable-reference check.
 func getRangeVarRefAnalyzer() *analysis.Analyzer {
 	return &analysis.Analyzer{
 		Name:     "rangeVarRef",
@@ -28,13 +30,26 @@ func getRangeVarRefAnalyzer() *analysis.Analyzer {
 	}
 }
 
-// checkRangeVarRef checks if a defer statement exists anywhere in a loop and flags it as an issue.
+// checkRangeVarRef checks if a range variable's reference is taken.
 func checkRangeVarRef(pass *analysis.Pass) (interface{}, error) {
 	type rangeVar struct {
 		name     string
 		scopeEnd token.Pos
 	}
-	rangeVars := make([]rangeVar, 0, expectedMaxNesting)
+	rangeVars := make([]rangeVar, 0, expectedMaxLoopNesting)
+
+	checkIfRangeVar := func(id *ast.Ident, pos token.Pos) {
+		for i := len(rangeVars) - 1; i >= 0; i-- {
+			r := rangeVars[i]
+			if id.Name == r.name && pos < r.scopeEnd {
+				pass.Reportf(pos,
+					"taking reference of range variable %s or its field",
+					id.Name)
+				break
+			}
+		}
+	}
+
 	visitor := func(node ast.Node) {
 		// cleanup rangeVars that node is out of bounds of
 		for i, r := range rangeVars {
@@ -55,25 +70,25 @@ func checkRangeVarRef(pass *analysis.Pass) (interface{}, error) {
 			if s.Op != token.AND {
 				return
 			}
-			var id *ast.Ident
-			switch i := s.X.(type) {
-			case *ast.Ident:
-				id = i
-			case *ast.SelectorExpr:
-				id, _ = i.X.(*ast.Ident)
-			}
-			if id == nil {
-				return
-			}
 
-			for _, r := range rangeVars {
-				if id.Name == r.name && s.Pos() < r.scopeEnd {
-					pass.Reportf(s.Pos(),
-						"taking reference of range variable %s or its field",
-						id.Name)
-					break
+			a := s.X
+			for {
+				switch id := a.(type) {
+				case *ast.Ident:
+					checkIfRangeVar(id, s.Pos())
+					return
+				case *ast.SelectorExpr:
+					if t, ok := pass.TypesInfo.Types[id.X]; ok {
+						if _, isPointer := t.Type.(*types.Pointer); isPointer {
+							return
+						}
+					}
+					a = id.X
+				default:
+					panic(fmt.Sprintf("Unknown expr type: %s", spew.Sdump(id)))
 				}
 			}
+
 		default:
 			// this shouldn't happen, the filter below shouldn't let this happen
 			panic(fmt.Sprintf("Unexpected statement of type %T received", s))
--- pkg/analyzer/testdata/src/range_val_ref/1.go
+++ pkg/analyzer/testdata/src/range_val_ref/1.go
@@ -1,16 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+type otherStruct struct {
+	b int
+}
 
 type myStruct struct {
 	f int
+	o *otherStruct
 }
 
-func main() {
-	lst := []myStruct{{1}, {2}, {3}}
+func lists() {
+	lst := []myStruct{}
 	for _, l := range lst {
 		fmt.Println("addr: %v", &l)   // want "taking reference of range variable l or its field"
-		fmt.Println("addr: %v", &l.f) // want "taking reference of range variable l or its field"
+		fmt.Println("addr: %v", &l.o) // want "taking reference of range variable l or its field"
 		for _, m := range lst {
 			fmt.Println("addr: %v", &m)   // want "taking reference of range variable m or its field"
 			fmt.Println("addr: %v", &m.f) // want "taking reference of range variable m or its field"
@@ -55,7 +63,29 @@ func main() {
 		fmt.Println("addr: %v", &l.f) // want "taking reference of range variable l or its field"
 	}
 
-	mp := map[int]int{}
+	for i, l := range os.Environ() {
+		fmt.Printf("addr: %v", &i) // want "taking reference of range variable i or its field"
+		fmt.Printf("addr: %v", &l) // want "taking reference of range variable l or its field"
+	}
+
+	for i, l := range os.Args {
+		fmt.Printf("addr: %v", &i) // want "taking reference of range variable i or its field"
+		fmt.Printf("addr: %v", &l) // want "taking reference of range variable l or its field"
+	}
+
+	for i, l := range []myStruct{} {
+		fmt.Printf("addr: %v", &i) // want "taking reference of range variable i or its field"
+		fmt.Printf("addr: %v", &l) // want "taking reference of range variable l or its field"
+	}
+
+	for i, l := range []*myStruct{} {
+		fmt.Printf("addr: %v", &i) // want "taking reference of range variable i or its field"
+		fmt.Printf("addr: %v", &l) // want "taking reference of range variable l or its field"
+	}
+}
+
+func maps() {
+	mp := map[myStruct]myStruct{}
 	for k1, v1 := range mp {
 		fmt.Println("addr: %v", &k1) // want "taking reference of range variable k1 or its field"
 		fmt.Println("addr: %v", &v1) // want "taking reference of range variable v1 or its field"
@@ -65,5 +95,74 @@ func main() {
 			fmt.Println("addr: %v", &k2) // want "taking reference of range variable k2 or its field"
 			fmt.Println("addr: %v", &v2) // want "taking reference of range variable v2 or its field"
 		}
+	}
+}
+
+func retListPtr() []*myStruct {
+	return nil
+}
+
+func pointers() {
+	lst := []myStruct{}
+	for i, l := range lst {
+		fmt.Printf("addr: %v", &i)     // want "taking reference of range variable i or its field"
+		fmt.Printf("addr: %v", &l)     // want "taking reference of range variable l or its field"
+		fmt.Printf("addr: %v", &l.o)   // want "taking reference of range variable l or its field"
+		fmt.Printf("addr: %v", &l.o.b) // This is ok. `o` is a pointer, so &l.o.f is same as &lst[i].o.f
+	}
+
+	plst := []*myStruct{}
+	for i, l := range plst {
+		fmt.Printf("addr: %v", &i)     // want "taking reference of range variable i or its field"
+		fmt.Printf("addr: %v", &l)     // want "taking reference of range variable l or its field"
+		fmt.Printf("addr: %v", &l.o)   // This is ok. `l` is a pointer, so &l.o is same as &lst[i].o
+		fmt.Printf("addr: %v", &l.o.b) // This is ok. `l` is a pointer, so &l.o.f is same as &lst[i].o.f
+	}
+
+	for i, l := range retListPtr() {
+		fmt.Printf("addr: %v", &i)     // want "taking reference of range variable i or its field"
+		fmt.Printf("addr: %v", &l)     // want "taking reference of range variable l or its field"
+		fmt.Printf("addr: %v", &l.o)   // This is ok. `l` is a pointer, so &l.o is same as &lst[i].o
+		fmt.Printf("addr: %v", &l.o.b) // This is ok. `l` is a pointer, so &l.o.f is same as &lst[i].o.f
+	}
+
+	mp1 := map[myStruct]myStruct{}
+	for k1, v1 := range mp1 {
+		fmt.Println("addr: %v", &k1)     // want "taking reference of range variable k1 or its field"
+		fmt.Println("addr: %v", &k1.o)   // want "taking reference of range variable k1 or its field"
+		fmt.Println("addr: %v", &k1.o.b) // This is ok. `o` is a pointer
+		fmt.Println("addr: %v", &v1)     // want "taking reference of range variable v1 or its field"
+		fmt.Println("addr: %v", &v1.o)   // want "taking reference of range variable v1 or its field"
+		fmt.Println("addr: %v", &v1.o.b) // This is ok. `o` is a pointer
+	}
+
+	mp2 := map[myStruct]*myStruct{}
+	for k1, v1 := range mp2 {
+		fmt.Println("addr: %v", &k1)     // want "taking reference of range variable k1 or its field"
+		fmt.Println("addr: %v", &k1.o)   // want "taking reference of range variable k1 or its field"
+		fmt.Println("addr: %v", &k1.o.b) // This is ok. `o` is a pointer
+		fmt.Println("addr: %v", &v1)     // want "taking reference of range variable v1 or its field"
+		fmt.Println("addr: %v", &v1.o)   // This is ok. `v1` is a pointer
+		fmt.Println("addr: %v", &v1.o.b) // This is ok. `v1` is a pointer
+	}
+
+	mp3 := map[*myStruct]myStruct{}
+	for k1, v1 := range mp3 {
+		fmt.Println("addr: %v", &k1)     // want "taking reference of range variable k1 or its field"
+		fmt.Println("addr: %v", &k1.o)   // This is ok. `k1` is a pointer
+		fmt.Println("addr: %v", &k1.o.b) // This is ok. `k1` is a pointer
+		fmt.Println("addr: %v", &v1)     // want "taking reference of range variable v1 or its field"
+		fmt.Println("addr: %v", &v1.o)   // want "taking reference of range variable v1 or its field"
+		fmt.Println("addr: %v", &v1.o.b) // This is ok. `o` is a pointer
+	}
+
+	mp4 := map[*myStruct]*myStruct{}
+	for k1, v1 := range mp4 {
+		fmt.Println("addr: %v", &k1)     // want "taking reference of range variable k1 or its field"
+		fmt.Println("addr: %v", &k1.o)   // This is ok. `k1` is a pointer
+		fmt.Println("addr: %v", &k1.o.b) // This is ok. `k1` is a pointer
+		fmt.Println("addr: %v", &v1)     // want "taking reference of range variable v1 or its field"
+		fmt.Println("addr: %v", &v1.o)   // This is ok. `v1` is a pointer
+		fmt.Println("addr: %v", &v1.o.b) // This is ok. `v1` is a pointer
 	}
 }
